9ded0ab86c1a4dbd53c1e1794bf75714ecf4673f
ANU <Satcorpvk@outlook.com>
2026-02-25 13:06:42 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,11 @@
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-[#050507] text-[#E5E7EB]">{children}</body>
+    </html>
+  );
+}
